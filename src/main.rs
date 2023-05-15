a7138a632ed71615c9d4d589d7327a4b04199e70
Patrice Tisserand <p.tisserand@gmail.com>
2023-05-15 18:50:54 +0200

    Add missing POST, PUT and DELETE routes implementation with actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::middleware::Logger;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{delete, get, post, put, web, App, HttpResponse, HttpServer, Responder, Result};
 use env_logger::Env;
-use log::{info, LevelFilter};
+use log::info;
 use std::sync::{Arc, Mutex};
 
 use tokio_postgres::{Client, NoTls};
@@ -20,33 +20,7 @@ struct User {
 // DATABASE URL
 const DB_URL: &str = env!("DATABASE_URL");
 
-/*
-fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    let mut request = String::new();
-
-    match stream.read(&mut buffer) {
-        Ok(size) => {
-            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
-
-            let (status_line, content) = match &*request {
-                r if r.starts_with("POST /users") => handle_post_request(r),
-                r if r.starts_with("GET /users/") => handle_get_request(r),
-                r if r.starts_with("GET /users") => handle_get_all_request(r),
-                r if r.starts_with("PUT /users/") => handle_put_request(r),
-                r if r.starts_with("DELETE /users/") => handle_delete_request(r),
-                _ => (NOT_FOUND.to_string(), "404 Not found".to_string())
-            };
-
-            stream.write_all(format!("{}{}", status_line, content).as_bytes()).unwrap();
-        }
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
-}
-*/
-
+// CONTROLLERS
 #[get("/users")]
 async fn get_users(db: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
     info!("Retrieving list of users");
@@ -63,114 +37,118 @@ async fn get_users(db: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
     HttpResponse::Ok().json(users)
 }
 
-/*
-// CONTROLLERS
-fn handle_post_request(request: &str) -> (String, String) {
-    match (
-        get_user_request_body(&request),
-        Client::connect(DB_URL, NoTls),
-    ) {
-        (Ok(user), Ok(mut client)) => {
-            client
-                .execute(
-                    "INSERT INTO users (name, email) VALUES ($1, $2)",
-                    &[&user.name, &user.email],
-                )
-                .unwrap();
-
-            (OK_RESPONSE.to_string(), "User created".to_string())
-        }
-        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+#[post("/users")]
+async fn create_user(body: web::Json<User>, db: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
+    info!("Create an user");
+    let client = db.lock().unwrap();
+    let user = body.into_inner();
+    let result = client
+        .query_one(
+            "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
+            &[&user.name, &user.email],
+        )
+        .await;
+    if result.is_ok() {
+        let id: i32 = result.unwrap().get(0);
+        info!("New id: {}", id);
+        let user = User {
+            id: Some(id),
+            name: user.name,
+            email: user.email,
+        };
+        HttpResponse::Created().json(user)
+    } else {
+        HttpResponse::InternalServerError().body("Failed to insert into DB")
     }
 }
 
-fn handle_get_request(request: &str) -> (String, String) {
-    match (
-        get_id(&request).parse::<i32>(),
-        Client::connect(DB_URL, NoTls),
-    ) {
-        (Ok(id), Ok(mut client)) => {
-            match client.query_one("SELECT * FROM users WHERE id = $1", &[&id]) {
-                Ok(row) => {
-                    let user = User {
-                        id: row.get(0),
-                        name: row.get(1),
-                        email: row.get(2),
-                    };
-                    (
-                        OK_RESPONSE.to_string(),
-                        serde_json::to_string(&user).unwrap(),
-                    )
-                }
-                _ => (NOT_FOUND.to_string(), format!("User {} not found", id)),
-            }
+#[get("/users/{id}")]
+async fn get_user(path: web::Path<String>, db: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
+    let client = db.lock().unwrap();
+    let path = path.into_inner();
+    let id = path.parse::<i32>();
+    if id.is_err() {
+        return HttpResponse::InternalServerError().body(format!("Can't parse {} as an id", path));
+    }
+    let id = id.unwrap();
+    info!("Retrieving user '{}'", id);
+
+    match client
+        .query_one("SELECT * FROM users WHERE id = $1", &[&id])
+        .await
+    {
+        Ok(row) => {
+            let user = User {
+                id: row.get(0),
+                name: row.get(1),
+                email: row.get(2),
+            };
+            HttpResponse::Ok().json(user)
         }
-        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+        _ => {
+            info!("User {} not found", id);
+            HttpResponse::NotFound().body(format!("User {} not found", id))
+        }
     }
 }
 
-fn handle_get_all_request(_request: &str) -> (String, String) {
-    match Client::connect(DB_URL, NoTls) {
-        Ok(mut client) => {
-            let mut users = Vec::new();
-
-            for row in client.query("SELECT * FROM users", &[]).unwrap() {
-                users.push(User {
-                    id: row.get(0),
-                    name: row.get(1),
-                    email: row.get(2),
-                });
-            }
-
-            (
-                OK_RESPONSE.to_string(),
-                serde_json::to_string(&users).unwrap(),
-            )
+#[put("/users/{id}")]
+async fn update_user(
+    path: web::Path<String>,
+    body: web::Json<User>,
+    db: web::Data<Arc<Mutex<Client>>>,
+) -> impl Responder {
+    let client = db.lock().unwrap();
+    let path = path.into_inner();
+    let mut user = body.into_inner();
+    let id = path.parse::<i32>();
+    if id.is_err() {
+        return HttpResponse::InternalServerError().body(format!("Can't parse {} as an id", path));
+    }
+    let id = id.unwrap();
+    let result = client
+        .execute(
+            "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+            &[&user.name, &user.email, &id],
+        )
+        .await;
+    if result.is_ok() {
+        let result = result.unwrap();
+        if result != 0 {
+            user.id = Some(id);
+            HttpResponse::Ok().json(user)
+        } else {
+            HttpResponse::NotFound().finish()
         }
-        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    } else {
+        return HttpResponse::InternalServerError().body(format!("Failed to update user {}", id));
     }
 }
 
-fn handle_put_request(request: &str) -> (String, String) {
-    match (
-        get_id(&request).parse::<i32>(),
-        get_user_request_body(&request),
-        Client::connect(DB_URL, NoTls),
-    ) {
-        (Ok(id), Ok(user), Ok(mut client)) => {
-            client
-                .execute(
-                    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
-                    &[&user.name, &user.email, &id],
-                )
-                .unwrap();
-
-            (OK_RESPONSE.to_string(), format!("User {} updated", id))
+#[delete("/users/{id}")]
+async fn delete_user(path: web::Path<String>, db: web::Data<Arc<Mutex<Client>>>) -> impl Responder {
+    let client = db.lock().unwrap();
+    let path = path.into_inner();
+    let id = path.parse::<i32>();
+    if id.is_err() {
+        return HttpResponse::InternalServerError().body(format!("Can't parse {} as an id", path));
+    }
+    let id = id.unwrap();
+    info!("Deleting user '{}'", id);
+    let rows_affected = client
+        .execute("DELETE FROM users WHERE id = $1", &[&id])
+        .await;
+    if rows_affected.is_ok() {
+        let rows_affected = rows_affected.unwrap();
+        if rows_affected == 0 {
+            HttpResponse::NotFound().body(format!("User {} not found", id))
+        } else {
+            HttpResponse::NoContent().finish()
         }
-        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
+    } else {
+        HttpResponse::InternalServerError().body("SQL query failed")
     }
 }
-
-fn handle_delete_request(request: &str) -> (String, String) {
-    match (
-        get_id(&request).parse::<i32>(),
-        Client::connect(DB_URL, NoTls),
-    ) {
-        (Ok(id), Ok(mut client)) => {
-            let rows_affected = client
-                .execute("DELETE FROM users WHERE id = $1", &[&id])
-                .unwrap();
-            if rows_affected == 0 {
-                return (NOT_FOUND.to_string(), format!("User {} not found", id));
-            }
-
-            (OK_RESPONSE.to_string(), format!("User {} deleted", id))
-        }
-        _ => (INTERNAL_SERVER_ERROR.to_string(), "Error".to_string()),
-    }
-}
-
-*/
 
 // main function
 #[actix_web::main]
@@ -191,6 +169,10 @@ async fn main() -> Result<(), std::io::Error> {
             .wrap(Logger::default())
             .app_data(web::Data::new(db_client.clone()))
             .service(get_users)
+            .service(create_user)
+            .service(get_user)
+            .service(update_user)
+            .service(delete_user)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
